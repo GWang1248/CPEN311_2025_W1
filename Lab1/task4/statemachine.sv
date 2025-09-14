d3f8b3d27d2574844360875697a2c5a81974b356
Wang <ivanwang1248@gmail.com>
2025-09-13 22:54:20 -0700

    Improved FSM other than DIVERGE part

--- Lab1/task4/statemachine.sv
+++ Lab1/task4/statemachine.sv
@@ -4,17 +4,8 @@ module statemachine(input logic slow_clock, input logic resetb,
                     output logic load_dcard1, output logic load_dcard2, output logic load_dcard3,
                     output logic player_win_light, output logic dealer_win_light);
 
-//Initialize
-    assign load_dcard1 = 0;
-    assign load_pcard1 = 0;
-    assign load_dcard2 = 0;
-    assign load_pcard2 = 0;
-    assign load_dcard3 = 0;
-    assign load_pcard3 = 0;
-
 //Define States
     logic [3:0] state;
-    logic [3:0] next_state;
 
     parameter IDLE = 4'b0000;
     parameter D1 = 4'b0001;
@@ -23,32 +14,46 @@ module statemachine(input logic slow_clock, input logic resetb,
     parameter P2 = 4'b0100;
     parameter DIVERGE = 4'b0101;
 
-    always_ff @(posedge slow_clock) begin //Beginning of State Machine
-        if (resetb == 0)
-            state = IDLE; //Reset State -> Idle
-        else begin
-            case (state)
-                IDLE: begin
-                    state = D1;
-                end
+//Statemachine Logic
+    always_ff @(posedge slow_clock) begin
+
+        if (resetb == 0) begin //Reset
+            load_dcard1 <= 0;
+            load_pcard1 <= 0;
+            load_dcard2 <= 0;
+            load_pcard2 <= 0;
+            load_dcard3 <= 0;
+            load_pcard3 <= 0;
+            state <= IDLE;
+        end
+
+        else begin //Begin Baccarat
+            load_dcard1 <= 0;
+            load_pcard1 <= 0;
+            load_dcard2 <= 0;
+            load_pcard2 <= 0;
+            load_dcard3 <= 0;
+            load_pcard3 <= 0;
+            case(state)
+                IDLE: state <= D1;
                 D1: begin
-                    state = P1;
-                    load_dcard1 = 1;
+                    state <= P1;
+                    load_dcard1 <= 1;
                 end
                 P1: begin
-                    state = D2;
-                    load_pcard1 = 1;
+                    state <= D2;
+                    load_pcard1 <= 1;
                 end
                 D2: begin
-                    state = P2;
-                    load_dcard2 = 1
+                    state <= P2;
+                    load_dcard2 <= 1;
                 end
                 P2: begin
-                    state = DIVERGE;
-                    load_pcard2 = 1
+                    state <= DIVERGE;
+                    load_pcard2 <= 1;
                 end
                 DIVERGE: begin
-                    //NOT FINISHED YET
+
                 end
             endcase
         end
