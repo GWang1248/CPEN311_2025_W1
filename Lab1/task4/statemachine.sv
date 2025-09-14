db5722b036c20b18114d55c87dd8867cf6290876
Wang <ivanwang1248@gmail.com>
2025-09-13 21:36:52 -0700

    Initializing State Machine

--- Lab1/task4/statemachine.sv
+++ Lab1/task4/statemachine.sv
@@ -4,10 +4,54 @@ module statemachine(input logic slow_clock, input logic resetb,
                     output logic load_dcard1, output logic load_dcard2, output logic load_dcard3,
                     output logic player_win_light, output logic dealer_win_light);
 
-// The code describing your state machine will go here.  Remember that
-// a state machine consists of next state logic, output logic, and the 
-// registers that hold the state.  You will want to review your notes from
-// CPEN 211 or equivalent if you have forgotten how to write a state machine.
+//Initialize
+    assign load_dcard1 = 0;
+    assign load_pcard1 = 0;
+    assign load_dcard2 = 0;
+    assign load_pcard2 = 0;
+    assign load_dcard3 = 0;
+    assign load_pcard3 = 0;
+
+//Define States
+    logic [3:0] state;
+    logic [3:0] next_state;
+
+    parameter IDLE = 4'b0000;
+    parameter D1 = 4'b0001;
+    parameter P1 = 4'b0010;
+    parameter D2 = 4'b0011;
+    parameter P2 = 4'b0100;
+    parameter DIVERGE = 4'b0101;
+
+    always_ff @(posedge slow_clock) begin //Beginning of State Machine
+        if (resetb == 0)
+            state = IDLE; //Reset State -> Idle
+        else begin
+            case (state)
+                IDLE: begin
+                    state = D1;
+                end
+                D1: begin
+                    state = P1;
+                    load_dcard1 = 1;
+                end
+                P1: begin
+                    state = D2;
+                    load_pcard1 = 1;
+                end
+                D2: begin
+                    state = P2;
+                    load_dcard2 = 1
+                end
+                P2: begin
+                    state = DIVERGE;
+                    load_pcard2 = 1
+                end
+                DIVERGE: begin
+                    //NOT FINISHED YET
+                end
+            endcase
+        end
+    end
 
 endmodule
-
