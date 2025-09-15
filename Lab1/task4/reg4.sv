d0008ee7ad81d46087ed465f1de21d9dd36ceb66
Wang <ivanwang1248@gmail.com>
2025-09-14 21:46:33 -0700

    Reset is working now

--- Lab1/task4/reg4.sv
+++ Lab1/task4/reg4.sv
@@ -4,8 +4,8 @@ module reg4(input logic [3:0] new_card,
 			input logic slow_clock,
 			output logic [3:0] final_card);
 
-	always_ff @(posedge slow_clock) begin
-		if (resetb == 0)
+	always_ff @(posedge slow_clock or negedge resetb) begin
+		if (!resetb)
 			final_card = 4'b0;
 		else begin
 			if (load_card == 1)
