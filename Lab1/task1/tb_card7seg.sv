c95393cbcf357880edc894151a94c96b1ea44ab7
jake19976 <148152100+jake19976@users.noreply.github.com>
2025-09-14 17:17:49 -0700

    Update tb_card7seg.sv

--- Lab1/task1/tb_card7seg.sv
+++ Lab1/task1/tb_card7seg.sv
@@ -1,17 +1,20 @@
 module tb_card7seg;
-    reg [3:0] card;  
-    wire [6:0] HEX0; 
+	reg [3:0] SW;  //can change this to simulate_SW or something
+	wire [6:0] HEX0; //same with this
 
-    card7seg hexdisp (.card(card), .HEX(HEX0)); //module instantiation 
+	//instantiate module
+	card7seg DUT(.SW(SW), .HEX0(HEX0)); 
 
-    //display output
-    initial begin
-        card = 0;
-        repeat (16) begin
-            $display ("card = %b  HEX0 = %b", card, HEX0);
-            card += 1; #20; 
-        end
-        $stop;
-    end
+	//output check
+	initial begin
+		SW=4'b0000;
+		for(int i=0; i<16; i++) begin  //check all 16 states
+			$display ("SW = %b HEX0 = %b", SW, HEX0);
+			SW+=i; 
+			#5;
+		end	
+	end
+
+
 endmodule
 
