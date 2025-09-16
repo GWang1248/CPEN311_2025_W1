3ef3ddf36765edf39779483b718ce5265fa19611
jake19976 <148152100+jake19976@users.noreply.github.com>
2025-09-16 03:39:04 -0700

    Update tb_scorehand.sv

--- Lab1/task5/tb_scorehand.sv
+++ Lab1/task5/tb_scorehand.sv
@@ -3,5 +3,24 @@ module tb_scorehand();
 // Your testbench goes here. Make sure your tests exercise the entire design
 // in the .sv file.  Note that in our tests the simulator will exit after
 // 10,000 ticks (equivalent to "initial #10000 $finish();").
-
+  logic[3:0] card1, card2, card3, total;
+  	
+  	//instantiate scorehand variables
+  	scorehand result(.card1, .card2, .card3, .total);
+  	
+  	initial begin
+  		for (int i = 0; i < 10; i++) begin
+  			
+  			card1 = $urandom_range(13,0);
+  			card2 = $urandom_range(13,0);
+  			card3 = $urandom_range(13,0);
+  			#1;
+  			if ( total==(card1 + card2 + card3)%10 ) begin
+  				$display("Card numbers added properly");
+  			end
+  			else begin
+  				$display("Card numbers failed to add properly");
+  			end
+  		end
+  	end
 endmodule
