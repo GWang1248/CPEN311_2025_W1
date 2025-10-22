8e2c1b9b59d36b5df0e9792e1dd1a93f55a7cb40
jake19976 <148152100+jake19976@users.noreply.github.com>
2025-10-21 17:50:09 -0700

    Update task2.sv

--- Lab2/task2/task2.sv
+++ Lab2/task2/task2.sv
@@ -8,5 +8,27 @@ module task2(input logic CLOCK_50, input logic [3:0] KEY,
              output logic [2:0] VGA_COLOUR, output logic VGA_PLOT);
 
     // instantiate and connect the VGA adapter and your module
+	logic [7:0] x;
+	logic [6:0] y;
+	logic [2:0] colour;
+	logic done;
+	logic plot;
+	logic start;
+	
+	fillscreen fs(.clk(CLOCK_50), .rst_n(KEY[3]), .start(start), .done(done), .vga_x(x), .vga_y(y), 
+	.vga_colour(colour), .vga_plot(plot));	
+	
+	vga_adapter adapter(.resetn(KEY[3]), .clock(CLOCK_50), .colour(colour), .x(x), .y(y), .plot(plot), .VGA_R(VGA_R),
+	.VGA_G(VGA_G), .VGA_B(VGA_B), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK(VGA_BLANK), .VGA_SYNC(VGA_SYNC), .VGA_CLK(VGA_CLK));
+
+	always_ff @(posedge CLOCK_50) begin
+		if(!KEY[3])
+			start = 1'b0;
+		else
+			start = 1'b1;
+		if (done)
+			start = 1'b0;
+	end
+		
 
 endmodule: task2
