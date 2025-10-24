e81bcb97e129e3f0edf8cda058c5a994ee2d18f1
Wang <ivanwang1248@gmail.com>
2025-10-23 18:05:20 -0700

    Circle Boundary Adding

--- Lab2/task4/reuleaux.sv
+++ Lab2/task4/reuleaux.sv
@@ -3,6 +3,24 @@ module reuleaux(input logic clk, input logic rst_n, input logic [2:0] colour,
                 input logic start, output logic done,
                 output logic [7:0] vga_x, output logic [6:0] vga_y,
                 output logic [2:0] vga_colour, output logic vga_plot);
-     // draw the Reuleaux triangle
-endmodule
 
+    parameter CONST = 11'd1732;
+    logic [7:0] centre_x1, centre_x2, centre_x3;
+    logic [6:0] centre_y1, centre_y2, centre_y3;
+
+    assign centre_x1 = centre_x + diameter / 2;
+    assign centre_x2 = centre_x - diameter / 2;
+    assign centre_x3 = centre_x;
+
+    assign centre_y1 = centre_y + diameter * CONST / 6000;
+    assign centre_y2 = centre_y + diameter * CONST / 6000;
+    assign centre_y3 = centre_y - diameter * CONST / 3000;
+
+    circle cr1(.clk(clk), .rst_n(rst_n), .colour(colour), .centre_x(centre_x1), .centre_y(centre_y1),
+                .radius(diameter), .start(start), .done(done), .vga_x(vga_x), .vga_y(vga_y), .vga_colour(vga_colour), .vga_plot(vga_plot));
+    //circle cr2(.clk(clk), .rst_n(rst_n), .colour(colour), .centre_x(centre_x2), .centre_y(centre_y2),
+                //.radius(diameter), .start(start), .done(done), .vga_x(vga_x), .vga_y(vga_y), .vga_colour(vga_colour), .vga_plot(vga_plot));
+    //circle cr3(.clk(clk), .rst_n(rst_n), .colour(colour), .centre_x(centre_x3), .centre_y(centre_y3),
+                //.radius(diameter), .start(start), .done(done), .vga_x(vga_x), .vga_y(vga_y), .vga_colour(vga_colour), .vga_plot(vga_plot));
+
+endmodule
--- Lab2/task4/task4.sv
+++ Lab2/task4/task4.sv
@@ -7,6 +7,25 @@ module task4(input logic CLOCK_50, input logic [3:0] KEY,
              output logic [7:0] VGA_X, output logic [6:0] VGA_Y,
              output logic [2:0] VGA_COLOUR, output logic VGA_PLOT);
 
-    // instantiate and connect the VGA adapter and your module
+        parameter diameter = 7'd80;
+        parameter centre_x = 8'd80;
+        parameter centre_y = 7'd60;
+
+    reuleaux rl(.clk(CLOCK_50), .rst_n(KEY[3]), .colour(colour), .centre_x(centre_x), .centre_y(centre_y),
+                .diameter(diameter), .start(start), .done(done), .vga_x(VGA_X), .vga_y(VGA_Y), .vga_colour(VGA_COLOUR), .vga_plot(VGA_PLOT));
+
+    vga_adapter adapter(.resetn(KEY[3]), .clock(CLOCK_50), .colour(VGA_COLOUR), .x(VGA_X), .y(VGA_Y), .plot(VGA_PLOT), .VGA_R(VGA_R),
+	                    .VGA_G(VGA_G), .VGA_B(VGA_B), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK(VGA_BLANK), .VGA_SYNC(VGA_SYNC), .VGA_CLK(VGA_CLK)); // Instantiate VGA Module
+
+    always_ff @(posedge CLOCK_50) begin
+        if(!KEY[3])
+            start <= 1'b0;
+        else begin
+          if(!done && !start)
+            start <= 1'b1;
+          if (done)
+            start <= 1'b0;
+        end
+    end
 
 endmodule: task4
