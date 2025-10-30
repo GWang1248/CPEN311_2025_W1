0232b0b5ec4a632077a4ef3722eaca915b535ee0
Wang <ivanwang1248@gmail.com>
2025-10-30 08:37:52 -0700

    Random rounds generator

--- Lab1/task5/tb_statemachine.sv
+++ Lab1/task5/tb_statemachine.sv
@@ -1,4 +1,15 @@
 `timescale 1ns/1ps
+
+//Randomizaition check macro
+`define SV_RAND_CHECK(r) \
+	do begin \
+		if (!(r)) begin \
+			$display("%s: %0d: Randomization failed \"%s\"", \
+			`__FILE__, `__LINE__, `"r`");\
+			$finish;\
+		end \
+	end while (0)
+
 //Defining a clocking block
 interface MyBus (input logic slow_clock);
     logic resetb;
@@ -9,13 +20,14 @@ interface MyBus (input logic slow_clock);
 
     //For this clocking block, we are driving dscore, pscore, pcard3 and resetb
     //We take the value of other inputs
-    //input in clocking block means output produced from the DUT
-    //output in clocking block means input given to the DUT
     clocking cb @(posedge slow_clock);
+
+        //input in clocking block means output produced from the DUT
         input load_pcard1, load_pcard2, load_pcard3;
         input load_dcard1, load_dcard2, load_dcard3;
         input player_win_light, dealer_win_light;
 
+        //output in clocking block means input given to the DUT
         output resetb, dscore, pscore, pcard3;
     endclocking
 endinterface
@@ -36,4 +48,25 @@ class RoundTrans;
     function display();
         $display("[%0t] pscore = %0d dscore = %0d pcard3 = %0d", $time, pscore, dscore, pcard3);
     endfunction
+endclass
+
+//Generate how many tests should be covered (default 10)
+//During each round generate different handle tr and put into mailbox
+//Which will be further delivered to Driver
+class Generator;
+    mailbox #(RoundTrans) mbox;
+
+    function new (mailbox #(RoundTrans) mbox);
+        this.mbox = mbox;
+    endfunction
+
+    task run(int rounds = 10);
+        RoundTrans tr;
+        repeat (rounds) begin
+            tr = new();
+            SV_RAND_CHECK(tr.randomize());
+            tr.display();
+            mbox.put(tr);
+        end
+    endtask
 endclass
