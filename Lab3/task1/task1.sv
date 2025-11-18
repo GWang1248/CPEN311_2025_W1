62e11cf230cca3b6a2c91a5df2c102bd26cec171
jake19976 <148152100+jake19976@users.noreply.github.com>
2025-11-18 13:19:24 -0800

    Update task1.sv

--- Lab3/task1/task1.sv
+++ Lab3/task1/task1.sv
@@ -5,7 +5,51 @@ module task1(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
 
     // your code here
 
-    s_mem s( /* connect ports */ );
+	logic rst_n;
+   	assign rst_n = KEY[3];
+
+	logic en, rdy, wren;
+	logic [7:0] addr, wrdata;
+	logic [7:0] s_q;    // read data from S memory
+	logic started; // to send only ONE en pulse after reset
+	logic[7:0] mem_addr;
+	
+	init dut(
+	.clk(CLOCK_50),
+	.rst_n(rst_n),
+	.en(en),
+	.rdy(rdy),
+	.addr(addr),
+	.wrdata(wrdata),
+	.wren(wren)
+	);
+	
+	
+	// address mux: when writing, use init's addr; otherwise use switches
+    	assign mem_addr = (wren) ? addr : SW[7:0];
+
+    	s_mem s( /* connect ports */
+		.address(mem_addr),
+		.clock(CLOCK_50),
+		.data(wrdata),
+		.wren(wren),
+		.q(s_q));
+	
+	
+	always_ff@(posedge CLOCK_50 or negedge rst_n) begin
+		if(!rst_n) begin
+			en <= 1'b0;
+			started <= 1'b0;
+		end 
+		else begin
+		if(!started && rdy) begin
+			en <= 1'b1;
+			started <= 1'b1; //allows the init to be initialized only once
+		end
+		else 
+			en <= 1'b0;
+		end
+	end				
 
     // your code here
 
