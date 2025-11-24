84e80e1ce1c056ae68aa962e4ce54e1a12b62d08
Wang <ivanwang1248@gmail.com>
2025-11-23 16:08:37 -0800

    Task 3 prga

--- Lab3/task3/prga.sv
+++ Lab3/task3/prga.sv
@@ -1,184 +1,159 @@
 module prga(input logic clk, input logic rst_n,
             input logic en, output logic rdy,
-            input logic [23:0] key, // key is not used in this module, but kept for interface consistency
+            input logic [23:0] key,
             output logic [7:0] s_addr, input logic [7:0] s_rddata, output logic [7:0] s_wrdata, output logic s_wren,
             output logic [7:0] ct_addr, input logic [7:0] ct_rddata,
             output logic [7:0] pt_addr, input logic [7:0] pt_rddata, output logic [7:0] pt_wrdata, output logic pt_wren);
 
-    typedef enum logic [4:0] {
+    typedef enum logic [5:0] {
         IDLE,
-        READ_LEN,
-        WRITE_LEN,
-        LOOP_START,
-        UPDATE_I,
-        READ_S_I,
-        UPDATE_J,
-        READ_S_J,
-        WRITE_S_I,
+        REQ_MSG_LEN,
+        GET_MSG_LEN,
+        WRITE_PT_0,
+        REQ_CT,
+        GET_CT,
+        UPD_I,
+        REQ_S_I,
+        GET_S_I,
+        S_I,
+        UPD_J,
+        REQ_S_J,
+        GET_S_J,
+        S_J,
         WRITE_S_J,
+        WRITE_S_I,
         REQ_PAD,
-        READ_PAD,
-        READ_CT_K,
+        GET_PAD,
         WRITE_PT,
-        DONE
-    } state_t;
+        LOOP} state_t;
 
     state_t state, next_state;
 
-    logic [7:0] i, j;
-    logic [8:0] k;
+    logic [7:0] i, j, k;
     logic [7:0] s_i, s_j;
-    logic [7:0] msg;
-    logic [7:0] pad;
-
-    logic [7:0] i_next, j_next;
-    logic [8:0] k_next;
-    logic [7:0] s_i_next, s_j_next;
-    logic [7:0] msg_next;
-    logic [7:0] pad_next;
+    logic [7:0] msg_len;
+    logic [7:0] pad, ct;
 
     always_ff @(posedge clk) begin
-        if (!rst_n) begin
+        if (!rst_n)
             state <= IDLE;
-            i <= 8'd0;
-            j <= 8'd0;
-            k <= 8'd0;
-            s_i <= 8'd0;
-            s_j <= 8'd0;
-            msg <= 8'd0;
-            pad <= 8'd0;
-        end
         else begin
             state <= next_state;
-            i <= i_next;
-            j <= j_next;
-            k <= k_next;
-            s_i <= s_i_next;
-            s_j <= s_j_next;
-            msg <= msg_next;
-            pad <= pad_next;
+            case (state)
+                IDLE: begin
+                    i <= 0;
+                    j <= 0;
+                    k <= 1;
+                    s_i <= 0;
+                    s_j <= 0;
+                    msg_len <= 0;
+                    pad <= 0;
+                    ct <= 0;
+                end
+                REQ_MSG_LEN: ;
+                GET_MSG_LEN: msg_len <= ct_rddata;
+                REQ_CT: ;
+                GET_CT: ct <= ct_rddata;
+                WRITE_PT_0: ;
+                UPD_I: i <= i + 1;
+                REQ_S_I: ;
+                GET_S_I: s_i <= s_rddata;
+                S_I: ;
+                UPD_J: j <= j + s_i;
+                REQ_S_J: ;
+                GET_S_J: s_j <= s_rddata;
+                S_J: ;
+                WRITE_S_J: ;
+                WRITE_S_I: ;
+                REQ_PAD: ;
+                GET_PAD: pad <= s_rddata;
+                WRITE_PT: ;
+                LOOP: begin
+                    if (k <= msg_len)
+                        k <= k + 1;
+                end
+                default: ;
+            endcase
         end
     end
 
-    // Next state logic and signal assignments
     always_comb begin
         next_state = state;
-        i_next = i;
-        j_next = j;
-        k_next = k;
-        s_i_next = s_i;
-        s_j_next = s_j;
-        msg_next = msg;
-        pad_next = pad;
-
+        s_addr = 0;
+		s_wrdata = 0;
+	    s_wren = 0;		
+		pt_addr = 0;
+		pt_wrdata = 0;
+		pt_wren = 0;			
+		ct_addr = 0;
         rdy = 1'b0;
-        s_addr = 8'd0;
-        s_wrdata = 8'd0;
-        s_wren = 1'b0;
-        ct_addr = 8'd0;
-        pt_addr = 8'd0;
-        pt_wrdata = 8'd0;
-        pt_wren = 1'b0;
-
         case (state)
             IDLE: begin
-                rdy = 1'b1;
                 if (en) begin
-                    i_next = 8'd0;
-                    j_next = 8'd0;
-                    k_next = 8'd1;
-                    next_state = READ_LEN;
+                    rdy = 0;
+                    next_state = REQ_MSG_LEN;
+                end
+                else begin
+                    rdy = 1;
+                    next_state = IDLE;
                 end
             end
-
-            READ_LEN: begin
-                ct_addr = 8'd0;
-                next_state = WRITE_LEN;
-            end
-
-            WRITE_LEN: begin
-                msg_next = ct_rddata;
-                pt_addr = 8'd0;
-                pt_wrdata = ct_rddata;
+            REQ_MSG_LEN: next_state = GET_MSG_LEN;
+            GET_MSG_LEN: next_state = WRITE_PT_0;
+            WRITE_PT_0: begin
                 pt_wren = 1'b1;
-                next_state = LOOP_START;
+                pt_wrdata = msg_len;
+                next_state = REQ_CT
             end
-
-            LOOP_START: begin
-                if (k > msg) begin
-                    next_state = DONE;
-                end else begin
-                    next_state = UPDATE_I;
-                end
+            REQ_CT: begin
+                ct_addr = k;
+                next_state = GET_CT;
             end
-
-            UPDATE_I: begin
-                i_next = (i + 8'b1) % 256;
-                s_addr = i_next;
-                next_state = READ_S_I;
+            GET_CT: next_state = UPD_I;
+            UPD_I: next_state = REQ_S_I;
+            REQ_S_I: begin
+                s_addr = i;
+                next_state = GET_S_I;
             end
-
-            READ_S_I: begin
-                s_i_next = s_rddata;
-                next_state = UPDATE_J;
+            GET_S_I: next_state = S_I;
+            S_I: next_state = UPD_J;
+            UPD_J: next_state = REQ_S_J;
+            REQ_S_J: begin
+                s_addr = j;
+                next_state = GET_S_J;
             end
-
-            UPDATE_J: begin
-                j_next = (j + s_i) % 256;
-                s_addr = j_next;
-                next_state = READ_S_J;
-            end
-
-            READ_S_J: begin
-                s_j_next = s_rddata;
+            GET_S_J: next_state = S_J;
+            S_J: next_state = WRITE_S_J;
+            WRITE_S_J: begin
+                s_wren = 1'b1;
+                s_addr = j;
+				s_wrdata = s_i;
                 next_state = WRITE_S_I;
             end
-
             WRITE_S_I: begin
+                s_wren = 1'b1;
                 s_addr = i;
                 s_wrdata = s_j;
-                s_wren = 1'b1;
-                next_state = WRITE_S_J;
-            end
-
-            WRITE_S_J: begin
-                s_addr = j;
-                s_wrdata = s_i;
-                s_wren = 1'b1;
                 next_state = REQ_PAD;
             end
-
             REQ_PAD: begin
-                s_addr = (s_i + s_j)  % 256;
-                next_state = READ_PAD;
+                s_addr = s_i + s_j;
+                next_state = GET_PAD;
             end
-
-            READ_PAD: begin
-                pad_next = s_rddata;
-                next_state = READ_CT_K;
-            end
-
-            READ_CT_K: begin
-                ct_addr = k;
-                next_state = WRITE_PT;
-            end
-
+            GET_PAD: next_state = WRITE_PT;
             WRITE_PT: begin
+                pt_wren = 1;
                 pt_addr = k;
-                pt_wrdata = pad ^ ct_rddata;
-                pt_wren = 1'b1;
-                k_next = k + 9'b1;
-                next_state = LOOP_START;
+				pt_wrdata = (pad ^ ct);
+                next_state = LOOP;
             end
-
-            DONE: begin
-                rdy = 1'b1;
-                next_state = DONE;
+            LOOP: begin
+                if (k <= msg_len)
+                    next_state = REQ_CT;
+                else
+                    next_state = IDLE;
             end
-
-            default: begin
-                next_state = IDLE;
-            end
+            default: ;
         endcase
     end
 
--- Lab3/task5/doublecrack.sv
+++ Lab3/task5/doublecrack.sv
@@ -5,12 +5,35 @@ module doublecrack(input logic clk, input logic rst_n,
 
     // your code here
     
+    typedef enum logic [4:0] {} state_t;
     // this memory must have the length-prefixed plaintext if key_valid
     pt_mem pt( /* connect ports */ );
 
     // for this task only, you may ADD ports to crack
-    crack c1( /* connect ports */ );
-    crack c2( /* connect ports */ );
+    crack c1(
+        .step(),
+        .start_key(),
+        .clk(CLOCK_50),
+        .rst_n(KEY[3]),
+        .en(en),
+        .rdy(rdy),
+        .key(c_key),
+        .key_valid(key_valid),
+        .ct_addr(ct_addr),
+        .ct_rddata(ct_rddata)
+    );
+    crack c2(
+        .step(),
+        .start_key(),
+        .clk(CLOCK_50),
+        .rst_n(KEY[3]),
+        .en(en),
+        .rdy(rdy),
+        .key(c_key),
+        .key_valid(key_valid),
+        .ct_addr(ct_addr),
+        .ct_rddata(ct_rddata)
+    );
     
     // your code here
 
